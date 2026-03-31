4babe4266fb5eed59e99e9687f51ac903b79e9b2
Shahd Hamdi <shahd.hamdi162@gmail.com>
2026-03-31 17:27:43 +0200

    Improve error handling and logger initialization

--- ingestion/csv_ingest.py
+++ ingestion/csv_ingest.py
@@ -5,17 +5,17 @@ import core.logger as logger
 class CSVIngest(Ingester):
     def __init__(self, file_path: str):
         self.file_path = file_path
+        self.audit_logger= logger.AuditLogger()
 
     def ingest(self):
-        audit_logger= logger.AuditLogger()
-        audit_logger.log_msg(f"Ingesting data from {self.file_path}...")
+        self.audit_logger.log_msg(f"Ingesting data from {self.file_path}...")
         try:
             data = dd.read_csv(self.file_path)
             if self.empty(data):
-                audit_logger.log_warning(f"{self.file_path} is empty")
+                self.audit_logger.log_warning(f"{self.file_path} is empty")
             else:
-                audit_logger.log_msg("Data ingested successfully!")
+                self.audit_logger.log_msg("Data ingested successfully!")
             return data
         except Exception as e:
-            audit_logger.log_err(f"An error occurred while ingesting data: {e}")
-            raise RuntimeError(f"Ingestion failed: {e}")
+            self.audit_logger.log_err(f"An error occurred while ingesting data: {e}")
+            return None
--- ingestion/json_ingest.py
+++ ingestion/json_ingest.py
@@ -4,18 +4,18 @@ import duckdb as dd
 class JSONIngest(Ingester):
     def __init__(self, file_path: str):
         self.file_path = file_path
+        self.audit_logger = logger.AuditLogger()
 
     def ingest(self):
-        audit_logger = logger.AuditLogger()
-        audit_logger.log_msg(f"ingesting data from {self.file_path}...")
+        self.audit_logger.log_msg(f"ingesting data from {self.file_path}...")
         try:
             data=dd.read_json(self.file_path, ignore_errors=True)
             if self.empty(data):
-                audit_logger.log_warning(f"{self.file_path} is empty")
+                self.audit_logger.log_warning(f"{self.file_path} is empty")
             else:
-                audit_logger.log_msg("Data ingested successfully!")
+                self.audit_logger.log_msg("Data ingested successfully!")
             return data
         except Exception as e:
-            audit_logger.log_err(f"An error occurred while ingesting data: {e}")
-            raise RuntimeError(f"Ingestion failed: {e}")
+            self.audit_logger.log_err(f"An error occurred while ingesting data: {e}")
+            return None
 
