955e71180966f7e6b9dc40160feee0d56d65a7f6
Mohammadtarekkk <mohammadtarekomar@gmail.com>
2026-04-01 05:10:33 +0200

    refactor: reintegrate schema validation and file processing tracking into the main pipeline

--- main.py
+++ main.py
@@ -1,36 +1,47 @@
+import os
 import threading
-import time
 from watchers.stream_watcher import StreamWatcher
 from watchers.batch_watcher import BatchWatcher
 from config.config_loader import Config
 from ingestion.ingester_factory import FactoryIngester
-from db.metadata_db import MetadataDB
+from db.metadata_db import MetadataTracker
+from config.schema_loader import SchemaLoader
+from processing.schema_validator import SchemaValidator
+
+app_config = Config()
+schema_loader = SchemaLoader(app_config.schema_path())
+metadata_tracker = MetadataTracker()
+validator = SchemaValidator(schema_loader)
 
 def pipeline_trigger(file_path):
+
+    if metadata_tracker.is_file_processed(file_path):
+        print(f"Skipping {file_path} (already processed)")
+        return
 
     file_type = file_path.split('.')[-1] 
     ingester = FactoryIngester(file_type).get_reader(file_path)
     try: 
         if ingester:
-            df=ingester.ingest()
-            if df is not None:
-                print(df.head())
+            relation = ingester.ingest()
+            if relation is not None:
+                valid_relation, _ = validator.validate_schema(file_path, relation)
+                if valid_relation:
+                    print(f"\nData validated, this is a sample:")
+                    print(valid_relation.limit(5))
+                    metadata_tracker.log_file_processed(file_path)
+                else:
+                    print(f"{file_path} failed Schema Validation. Dropping file.")
     except Exception as e:
-        print(f"An error occurred while ingesting the file: {e}")
-
-    
-
-
+        print(f"An error occurred while processing the file: {e}")
 
 
 class Main:
     def __init__(self):
-        self.app_config = Config()
         
         # get the paths from config.yaml  using config_loader.py
-        stream_path = self.app_config.stream_input_path()
-        batch_path = self.app_config.batch_input_path()
-        schema_path = self.app_config.schema_path()
+        stream_path = app_config.stream_input_path()
+        batch_path = app_config.batch_input_path()
 
         # create the watchers
         self.stream_watch = StreamWatcher(stream_path, pipeline_trigger)
@@ -46,25 +57,6 @@ class Main:
         # start them at the same time
         t1.start()
         t2.start()
-
-        try:
-            while True:
-                time.sleep(1)   # keep main thread alive
-                print("main_thread")
-        except KeyboardInterrupt:
-            print("\nCtrl+C caught — shutting down...")
-
-            # Stop watchers properly
-            self.stream_watch.stop()
-            self.batch_watch.stop()
-
-            # Wait for threads to exit
-            t1.join()
-            t2.join()
-
-
-            print("Shutdown complete.")
-
 
 
 if __name__ == "__main__":
