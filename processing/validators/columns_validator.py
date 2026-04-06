b2319386edb49e1050bebedd7a49a7c961c05370
omar ayman <omar.a.farouk22@gmail.com>
2026-04-06 02:22:58 +0200

    fix: update the column validator to compare count of cols with actual schema

--- processing/validators/columns_validator.py
+++ processing/validators/columns_validator.py
@@ -13,11 +13,6 @@ class ColumnsValidator(BaseValidator):
         required_cols = self.loader.get_required_cols(table_name)
         current_cols = relation.columns
 
-
-        required_lower = [c.lower() for c in required_cols]
-        actual_lower = [c.lower() for c in current_cols]
-
-
         # Count check
         if len(actual_lower) != len(required_lower):
             self.audit_logger.log_err(
@@ -25,17 +20,7 @@ class ColumnsValidator(BaseValidator):
                 f"expected {len(required_lower)}, got {len(actual_lower)}"
             )
             return False
-        
-        # check for missing columns
-        for col in required_lower:
-            if col not in actual_lower:
                 self.audit_logger.log_err(f"CRITICAL: '{table_name}' is missing required field: '{col}'. Dropping file.")
-                return False
-            
-        # check for extra columns
-        for col in actual_lower:
-            if col not in required_lower:
-                self.audit_logger.log_err(f"CRITICAL: '{table_name}' has extra column: '{col}'. Dropping file.")
                 return False
                 
         self.audit_logger.log_msg(f"SUCCESS: '{table_name}' passed Columns Validation.")
