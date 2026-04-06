f7efd4ce12a6eb5f2f48552bf244c0ffb56ee65b
omar ayman <omar.a.farouk22@gmail.com>
2026-04-06 02:13:33 +0200

    fix: attach reading json files to our singleton database connection instead og global duck db env

--- ingestion/json_ingest.py
+++ ingestion/json_ingest.py
@@ -1,26 +1,25 @@
-import os
-
-from db.connections import DuckDBConnection
-
 from .base_ingester import Ingester
 import core.logger as logger
 import duckdb as dd
+from db.connections import DuckDBConnection 
+
+
 class JSONIngest(Ingester):
     def __init__(self, file_path: str):
         self.file_path = file_path
         self.audit_logger = logger.AuditLogger()
-        self.duckdb = DuckDBConnection().conn
+        self.conn = DuckDBConnection().conn
 
     def ingest(self):
         self.audit_logger.log_msg(f"ingesting data from {self.file_path}...")
         try:
-            data = self.duckdb.read_json(self.file_path)
+            data = self.conn.read_json(self.file_path, timestamp_format = '%Y-%m-%d %H:%M:%S')
             if self.empty(data):
                 self.audit_logger.log_warning(f"{self.file_path} is empty")
-                return {'data':data, 'is_empty':True}
             else:
                 self.audit_logger.log_msg("Data ingested successfully!")
-            return {'data':data, 'is_empty':False}
+            return data
         except Exception as e:
             self.audit_logger.log_err(f"An error occurred while ingesting data: {e}")
-            return {'data':None, 'is_empty':False}
+            return None
+
