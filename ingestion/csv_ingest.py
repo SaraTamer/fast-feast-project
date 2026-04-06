92c093cf483ee58ad39729bacef3aa7ed09a5b88
omar ayman <omar.a.farouk22@gmail.com>
2026-04-06 02:13:09 +0200

    fix: attach reading csv files to our singleton database connection instead og global duck db env

--- ingestion/csv_ingest.py
+++ ingestion/csv_ingest.py
@@ -1,5 +1,3 @@
-import os
-
 from .base_ingester import Ingester
 import duckdb as dd
 import core.logger as logger
@@ -9,20 +7,17 @@ class CSVIngest(Ingester):
     def __init__(self, file_path: str):
         self.file_path = file_path
         self.audit_logger= logger.AuditLogger()
-        self.duckdb = DuckDBConnection().conn
+        self.conn = DuckDBConnection().conn
 
     def ingest(self):
         self.audit_logger.log_msg(f"Ingesting data from {self.file_path}...")
         try:
-            data = self.duckdb.read_csv(self.file_path)
+            data = self.conn.read_csv(self.file_path)
             if self.empty(data):
                 self.audit_logger.log_warning(f"{self.file_path} is empty")
-                return {'data': data, 'is_empty': True}
             else:
                 self.audit_logger.log_msg("Data ingested successfully!")
-            return {'data':data, 'is_empty':False}
+            return data
         except Exception as e:
             self.audit_logger.log_err(f"An error occurred while ingesting data: {e}")
-            return {'data':None, 'is_empty':False}
-
-
+            return None
