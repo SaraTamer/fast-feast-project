dad112839c0dcf3df6114b3476a0b799678ad077
omar ayman <omar.a.farouk22@gmail.com>
2026-04-07 23:19:37 +0200

    feat: add the format checker in main

--- main.py
+++ main.py
@@ -1,115 +1,90 @@
-import time
 import threading
-from watchers.stream_watcher import StreamWatcher
+import time
+
+from pipelines.batch_pipeline import BatchPipeline
+from pipelines.stream_pipeline import StreamPipeline
+
 from watchers.batch_watcher import BatchWatcher
+from watchers.stream_watcher import StreamWatcher
+
+from core.logger import AuditLogger
+
 from config.config_loader import Config
-from ingestion.ingester_factory import FactoryIngester
-from db.metadata_db import MetadataTracker
 from config.schema_loader import SchemaLoader
 from processing.schema_validator import SchemaValidator
 from processing.formats import FormatChecker      
 
-class PipelinePhases:
-    def __init__(self, metadata_tracker, validator, format_checker):
-        self.metadata_tracker = metadata_tracker
-        self.validator = validator
-        self.format_checker   = format_checker
-        self.processing_files = set()
-        self.lock = threading.Lock()        # thread-safe since you use 2 threads; where we will lock on shared variables when we use them for each thread
-
-    def pipeline_trigger(self, file_path):
-        with self.lock:
-            if file_path in self.processing_files:
-                print(f"Skipping {file_path} (currently being processed)")
-                return
-            # if self.metadata_tracker.is_file_processed(file_path):
-            #     print(f"Skipping {file_path} (already processed)")
-            #     return
-            # Mark as in-progress
-            self.processing_files.add(file_path)
-
-        file_type = file_path.split('.')[-1]
-        ingester = FactoryIngester(file_type).get_reader(file_path)
-        try:
-            if ingester:
-                relation = ingester.ingest()
-                if relation is not None:
-                    validated_relation, columns_meta = self.validator.validate_schema(file_path, relation)
-                    if validated_relation:
-                        print(f"\nData validated, this is a sample:")
-                        print(validated_relation.limit(5))
-                        self.metadata_tracker.log_file_processed(file_path)
-                    else:
-                        print(f"{file_path} failed Schema Validation. Dropping file.")
-                        return
-
-                    formatted_relation, bad_rows_df = self.format_checker.separate(
-                        validated_relation, columns_meta
-                    )
-                    print(f"{file_path} failed Schema Validation. Dropping file.")
+from db.metadata_db import MetadataTracker
+from caching.DimensionCache import DimensionCache
 
 
-        except Exception as e:
-            print(f"An error occurred while processing the file: {e}")
-        finally:
-            # Always remove from set — success or failure
-            with self.lock:
-                self.processing_files.discard(file_path)
-                print(f"Released {file_path} — ready to accept again")
+class PipelineApp:
 
-class Pipeline:
-    def __init__(self, app_config, phases):
-        self.app_config = app_config
-        self.phases = phases
-        
-        # get paths from config
-        self.stream_path = self.app_config.stream_input_path()
-        self.batch_path = self.app_config.batch_input_path()
+    def __init__(self):
 
-        # create watchers
-        self.stream_watch = StreamWatcher(self.stream_path, self.phases.pipeline_trigger)
-        self.batch_watch = BatchWatcher(self.batch_path, self.phases.pipeline_trigger)
-        
+        self.logger = AuditLogger()
+        config = Config()
+        schema_loader = SchemaLoader(config.schemas_path())
+        validator = SchemaValidator(schema_loader)
+        format_checker  = FormatChecker()
+        metadata = MetadataTracker()
+        self.dim_cache = DimensionCache()
+        self.batch_pipeline = BatchPipeline(
+            metadata,
+            validator,
+            format_checker,
+            self.dim_cache
+        )
+
+        self.stream_pipeline = StreamPipeline(
+            metadata,
+            validator,
+            format_checker,
+            self.dim_cache
+        )
+
+        self.batch_watcher = BatchWatcher(
+            config.batch_input_path(),
+            self.batch_pipeline.process_file
+        )
+
+        self.stream_watcher = StreamWatcher(
+            config.stream_input_path(),
+            self.stream_pipeline.process_event
+        )
+
         self.t1 = None
         self.t2 = None
 
     def start(self):
-        print("starting both Watchers with Multi-threading\n")
-        
-        # create two parallel threads
-        self.t1 = threading.Thread(target=self.stream_watch.watch_dog)
-        self.t2 = threading.Thread(target=self.batch_watch.watch_dog)
 
-        # start them at the same time
+        self.logger.log_msg("Starting Batch + Stream pipelines")
+
+        self.t1 = threading.Thread(target=self.batch_watcher.watch_dog)
+        self.t2 = threading.Thread(target=self.stream_watcher.watch_dog)
+
         self.t1.start()
         self.t2.start()
 
+
     def stop(self):
         print("\nCtrl+C caught — shutting down...")
-        
+
         # Stop watchers properly
-        self.stream_watch.stop()
-        self.batch_watch.stop()
+        self.stream_watcher.stop()
+        self.batch_watcher.stop()
 
         # Wait for threads to exit
         if self.t1: self.t1.join()
         if self.t2: self.t2.join()
-        
-        print("Shutdown complete.")
 
+        print("Shutdown complete.")
 
 class MainApp:
     def __init__(self):
-        # Initialize dependencies
-        self.app_config = Config()
-        self.schema_loader = SchemaLoader(self.app_config.schema_path())
-        self.metadata_tracker = MetadataTracker()
-        self.validator = SchemaValidator(self.schema_loader)
-        self.format_checker  = FormatChecker() 
 
         # Initialize core components
-        self.phases = PipelinePhases(self.metadata_tracker, self.validator, self.format_checker)
-        self.pipeline = Pipeline(self.app_config, self.phases)
+        self.pipeline = PipelineApp()
 
     def run(self):
         self.pipeline.start()
@@ -118,7 +93,6 @@ class MainApp:
                 time.sleep(1) # Keep main thread alive
         except KeyboardInterrupt:
             self.pipeline.stop()
-
 
 if __name__ == "__main__":
     app = MainApp()
